ac58c00e83144d5bbe0600c4506bf84dc4d7b9b1
DorsD <fabiola.gonzalez.fajardo@gmail.com>
2020-09-04 13:45:46 -0500

    Router routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from "react-router-dom"; 
+import Login from './Views/Login'
+import UserProfile from './Views/UserProfile'
+import Welcome from './Views/Welcome'
 
 console.log('config', process.env.config) 
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Welcome} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/profile" component={UserProfile} />
+      </Switch>
+    </BrowserRouter> 
   );
 }
 
