2697ab11955999de06c9de4e1b6c280ebe409496
Rosa <rosaislp@gmail.com>
2020-09-04 20:17:26 -0500

    rendering api names

--- src/Views/Welcome.js
+++ src/Views/Welcome.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+// import ReCaptcha from '../Components/ReCaptcha';
+import axios from 'axios';
 
 const Welcome = () => {
+
+  const [ data, setData ] = useState([]);
+
+  useEffect(() => {
+    
+    const getData = async () => {
+      let api = process.env.REACT_APP_BREAKINGBAD
+      const response = await axios.get(api)
+      setData(response.data)
+
+    }
+    getData()
+  }, [])
+  console.log('data', data);
+
   return (
     <div>
         Welcome to our app 
+        {/* <ReCaptcha
+          sitekey="your_site_key"
+          action='action_name'
+        /> */}
+      {data.map((item) => { 
+        return<p> {item.name}</p>
+        })}
     </div>
   )
 }
