9c337e209576556b61a3b49b3266d87ab0e3d0b8
DorsD <fabiola.gonzalez.fajardo@gmail.com>
2020-09-05 16:08:06 -0500

    env var fixed

--- src/Views/Welcome.js
+++ src/Views/Welcome.js
@@ -1,33 +1,11 @@
 import React, { useState, useEffect } from 'react';
 // import ReCaptcha from '../Components/ReCaptcha';
-import axios from 'axios';
 
 const Welcome = () => {
 
-  const [ data, setData ] = useState([]);
-
-  useEffect(() => {
-    
-    const getData = async () => {
-      let api = process.env.REACT_APP_BREAKINGBAD
-      const response = await axios.get(api)
-      setData(response.data)
-
-    }
-    getData()
-  }, [])
-  console.log('data', data);
-
   return (
     <div>
-        Welcome to our app 
-        {/* <ReCaptcha
-          sitekey="your_site_key"
-          action='action_name'
-        /> */}
-      {data.map((item) => { 
-        return<p> nombre: {item.name} </p>
-        })}
+      Welcome to our app 
     </div>
   )
 }
