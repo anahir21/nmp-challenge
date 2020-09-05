7608f9c61c45507057441ca4a087fc4d9fef6fd2
DorsD <fabiola.gonzalez.fajardo@gmail.com>
2020-09-04 19:05:54 -0500

    login inputs

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import dotenv from 'dotenv' 
+//import dotenv from 'dotenv' 
 
 import React from 'react';
-import { BrowserRouter, Switch, Route } from "react-router-dom"; 
+import { BrowserRouter, Switch, Route , NavLink} from "react-router-dom"; 
 import Login from './Views/Login'
 import UserProfile from './Views/UserProfile'
 import Welcome from './Views/Welcome'
-dotenv.config() 
-console.log('CONFIG', process.env.CONFIG) 
+//dotenv.config() 
+//require('dotenv').config();
+
+console.log(process.env.ALGO) 
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Welcome} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/profile" component={UserProfile} />
-      </Switch>
-    </BrowserRouter> 
+    <div>
+      <BrowserRouter>
+        <nav>
+          <NavLink  to='/'>
+            <input type="button" value='Home'/>
+          </NavLink>
+          <NavLink  to='/login'>
+            <input type="button" value='Login'/>
+          </NavLink>
+          <NavLink  to='/profile'>
+            <input type="button" value='Profile'/>
+          </NavLink>
+        </nav>
+      
+        <Switch>
+          <Route exact path="/" component={Welcome} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/profile" component={UserProfile} />
+        </Switch>
+      </BrowserRouter> 
+    </div>
   );
 }
 
--- src/Views/Login.js
+++ src/Views/Login.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Login = () => {
-    return (
-        <div>
-          Login                  
-        </div>
-    )
+
+  const[username, setUsername] = useState("");
+  const[password, setPassword] = useState("");
+
+  function loginBtn(){
+    console.log('Hey', username, password);
+  }
+
+  return (
+    <div>
+      <input type="text" 
+        id="loginUser" 
+        name="" 
+        placeholder='Username'
+        onChange={(e)=>setUsername(e.target.value)}  
+      />
+      <input type="password" 
+        id="loginPsw" name="" 
+        placeholder='Password'
+        onChange={(e)=>setPassword(e.target.value)}  
+      />
+      <input type="button" 
+        id="loginBtn" 
+        value="Enter" 
+        onClick={loginBtn}
+      />                
+    </div>
+  )
 }
 
 export default Login;
