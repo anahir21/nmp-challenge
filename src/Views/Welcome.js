b5d4b96f7a9b90604408586d9b5bf98eb7e673fc
DorsD <fabiola.gonzalez.fajardo@gmail.com>
2020-09-06 18:22:16 -0500

    delete api BB experiment

--- src/Views/Welcome.js
+++ src/Views/Welcome.js
@@ -4,16 +4,7 @@ import Header from '../Components/Header';
 import Form from '../Components/Form';
 
 const Welcome = () => {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    const getData = async () => {
-      let api = process.env.REACT_APP_BREAKINGBAD
-      const response = await axios.get(api)
-      setData(response.data)
-    }
-    getData()
-  }, [])
-  console.log('data', data);
+  
   return ( 
     <div >
       <Header />
