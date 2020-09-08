6db938b057f754db77d203bea77dfef59e9c06da
Rosa <rosaislp@gmail.com>
2020-09-07 23:51:59 -0500

    saves styles into form component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ import UserProfile from './Views/UserProfile'
 import Welcome from './Views/Welcome'
 import firebase from './Firebase/firebase'
 
+
+console.log(process.env.REACT_APP_FIREBASE_API_KEY);
+console.log(process.env.REACT_APP_FIREBASE_AUTH_DOMAIN);
+console.log(process.env.REACT_APP_FIREBASE_DATABASE_URL);
+console.log(process.env.REACT_APP_FIREBASE_PROJECT_ID);
+console.log(process.env.REACT_APP_FIREBASE_STORAGE_BUCKET);
+console.log(process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID);
+console.log(process.env.REACT_APP_FIREBASE_APP_ID);
+console.log(process.env.REACT_APP_PRUEBA);
+
 function App() {
 
   const[isLogin, setIsLogin] = useState(false)
