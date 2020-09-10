a7d098c1f3d453d3c6aa59395079a6f3dedfa9de
Rosa <rosaislp@gmail.com>
2020-09-10 09:55:50 -0500

    finish with Modal component

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
+import closeimg from "../Static/Images/close.png";
 import "../Styles/Modal.css";
 
 import recruiter from "../Static/Images/recruiter.png";
@@ -19,35 +20,34 @@ export default ({ title, children, isOpened, onClose }) =>
                     src={recruiter}
                     className="img-recruiter"
                   />
-                  <div>
-                    <p>hola</p>
-                    <p>otro</p>
+                  <div className="wrapper-recruitername-position">
+                    <p className="p-recruiter-full-name">Alexander Domínguez</p>
+                    <p>Analista de Atracción de Talento</p>
                   </div>
                 </div>
                 <div className="wrapper-recruiter-information">
                   <p className="p-recruiter-title">¡Hola, soy Alexander!</p>
-                  <p>
+                  <p className="p-recruiter-details">
                     Me considero una persona alegre, entusiasta y analítica, con
                     gran <br /> interés en el desarrollo de las personas y su
                     talento. <br />
                     <br />
-                    Trabajo en Nacional Monte de Piedad, por ser una empresa muy{" "}
-                    <br /> humana, con la noble labor de ayudar a quien más lo
-                    necesita. <br /> <br />
+                    <b>Trabajo en Nacional Monte de Piedad</b>, por ser una
+                    empresa muy <br /> humana, con la noble labor de ayudar a
+                    quien más lo necesita. <br /> <br />
                     Mi labor consiste en buscar, identificar y potencializar el
                     talento <br /> de las personas. <br /> <br />
-                    Hobbies: Me encanta el mundo del automovilismo (soy fan de
-                    la <br /> Fórmula 1 y la Fórmula E). Practico fútbol,
+                    <b>Hobbies</b>: Me encanta el mundo del automovilismo (soy
+                    fan de la <br /> Fórmula 1 y la Fórmula E). Practico fútbol,
                     natación y yoga.
                   </p>
                 </div>
-                <div>
-                  <button onClick={onClose}>
-                    <span aria-hidden="true">&times;</span>
+                <div className="container-btn-img-close">
+                  <button onClick={onClose} className="btn-close">
+                    <img alt="close" src={closeimg} />
                   </button>
                 </div>
               </div>
-              {/* <div className="modal-body">{children}</div> */}
             </div>
           </div>
         </div>,
