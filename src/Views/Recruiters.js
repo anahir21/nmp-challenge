9aa44572337e32dfc739ccdde2c865aad8fe3f7a
MiriamNM <miriamnavamucino@gmail.com>
2021-05-28 16:14:49 -0500

    history recruiter

--- src/Views/Recruiters.js
+++ src/Views/Recruiters.js
@@ -1,8 +1,115 @@
-import React from 'react'
+import React, {useEffect, useState}  from 'react'
+import ShortHeader from '../Components/ShortHeader';
+import SmallFooter from '../Components/SmallFooter';
+import TableRecluter from '../Components/TableRecluter';
+// import WelcomeUser from '../Components/WelcomeUser';
+import firebase from '../Firebase/firebase'
+import 'firebase/firestore'
+import '../Styles/profile.css'
+import '../Styles/RecruiterView.css'
+
+const db = firebase.firestore();
 
 const Recruiters = () => {
+  const [user, setUser] = useState({});
+  const [recruiter, setRecruiter] = useState({})
+  const [value, setValue] = useState();
+  
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
+
+  useEffect(() => {
+    const user = firebase.auth().currentUser;
+    if (user != null) {
+        console.log(user.uid);
+        
+        db.collection("users").where("UID", "==", user.uid).get()
+        .then((querySnapshot)=> {
+          querySnapshot.forEach((doc) => {
+            console.log(doc.id, " => ", doc.data());
+            setUser(doc.data())
+            db.collection("recruiters").where("id", "==", doc.data().recruiterID).get()
+            .then((querySnapshot)=> {
+              querySnapshot.forEach((doc) => {
+                console.log(doc.id, " => ", doc.data());
+                setRecruiter(doc.data())
+              })
+            })      
+          });
+        })
+        .catch(function(error) {
+          console.log("Error getting documents: ", error);
+        }); 
+    }
+  }, [])
+
+  const registerCandidateBtn = () => {
+    console.log('Btn Register User');
+    }
+  
+
   return (
-    <div>Miri y Denn</div>
+    <div className='profile-container'>
+      <ShortHeader />
+      {/* <div className='welcome-user'>
+        <WelcomeUser 
+          user={user}
+        /> 
+        <p className="welcomeUser-title">
+				¡Hola, {user.name}!
+			  </p>
+      </div>       */}
+      <div className='displayDataCandidates'>
+      <div>
+        <p className="welcomeUser-title">
+				  ¡Hola, {user.name}!
+			  </p>
+        <select
+          className ="selectFilter"
+          type="number"
+          onChange={(e) => {
+          setValue(e.target.value);
+          }}>
+          <option value="">FILTRAR POR STATUS</option>
+          <option value="SE">CV Y SOLICITUD DE EMPLEO</option>
+          <option value="EVA">EVALUACIÓN</option>
+          <option value="ENT">ENTREVISTAS</option>
+          <option value="DOC">DOCUMENTACIÓN</option>
+          <option value="EXME">EXAMEN MÉDICO</option>
+          <option value="ESOE">ESTUDIO SOCIO-ECONÓMICO</option>
+        </select>
+        <select
+          className ="selectFilter"
+          type="number"
+          onChange={(e) => {
+          setValue(e.target.value);
+          }}>
+          <option value="">FILTRAR POR VACANTE</option>
+          <option value="BI">BUSSINESS INTELLIGENCE</option>
+          <option value="TI">TECNOLOGÍA DE LA INF./SISTEMAS</option>
+          <option value="EC">ESTRATEGÍA COMERCIAL</option>
+          <option value="OPP">OPERACIÓN PRENDARIA</option>
+          <option value="SEG">SERVICIOS GENERALES</option>
+          <option value="CPR">CONTROL PRESUPUESTAL</option>
+          <option value="AUD">AUDITORÍA</option>
+          <option value="PRD">PRODUCTOS</option>
+          <option value="PFI">PLANEACIÓN FINANCIERA</option>
+        </select>
+
+        <input type='button' 
+          id=''
+          className='close-btn' 
+          value='REGISTRAR NUEVO CANDIDATO'
+          onClick={registerCandidateBtn} 
+        />
+        
+      </div>
+      <TableRecluter />
+      </div>
+      <SmallFooter />   
+    </div> 
+  
   )
 }
 
