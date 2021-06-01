ece2845dba8819cd02b98569e3816bc915eee3f4
Denisse <ledennii@gmail.com>
2021-06-01 14:04:21 -0500

    First try

--- src/Views/Recruiters.js
+++ src/Views/Recruiters.js
@@ -10,14 +10,36 @@ import ModalPreSingup from '../Components/ModalPreSignup';
 import firebase from '../Firebase/firebase'
 import 'firebase/firestore'
 import '../Styles/RecruiterView.css'
+import { database } from 'firebase';
 
-
-const db = firebase.firestore();
 
 const Recruiters = () => {
+
+ 
+  const db = firebase.firestore();
+const candidatesRef = db.collection('candidates');
+
   const [user, setUser] = useState({});
   const [recruiter, setRecruiter] = useState({})
   const [value, setValue] = useState();
+  const [postulants, setPostulants] = useState([]); //MIRI
+
+  const datas = () => {candidatesRef.onSnapshot((querySnapshot)=> {//MIRI
+    const docs = [];
+    querySnapshot.forEach(async(doc) => {
+       docs.push({ ...doc.data() });
+    })
+    setPostulants(docs);
+    console.log(docs, 'datos');
+  })
+}
+
+  useEffect(() =>{
+    datas();
+  }, []);//MIRI
+
+  console.log(datas, 'soy data miri')//MIRI
+
   
   useEffect(() => {
     window.scrollTo(0, 0)
@@ -52,6 +74,38 @@ const Recruiters = () => {
     openModal(ModalPreSingup);
   }
   
+  let lastDocument = null;
+  const NextPage = () => {
+    // console.log('Siguiente pag.')
+     const query = candidatesRef
+                    .orderBy('state')
+                    .startAfter( lastDocument )
+     query.limit(10).get().then(snap => {
+           firstDocument = snap.docs[ 0 ] || null;
+           lastDocument = snap.docs[ snap.docs.length -1] || null;
+           TableRecluter(snap); 
+           console.log('Si llegue')       
+     })              
+  }
+  let firstDocument = null;
+  const PrevPage = () => {
+    // console.log('Siguiente pag.')
+     const query = candidatesRef
+                    .orderBy('state')
+                    .endBefore( firstDocument )
+     query.limit(10).get().then(snap => {
+           firstDocument = snap.docs[ 0 ] || null;
+           lastDocument = snap.docs[ snap.docs.length -1] || null;
+           TableRecluter(snap);    
+           console.log('Si llegue 2 ')     
+     })              
+  }
+  // const NextPage = () => {
+  //   console.log('Siguiente pag.')
+  // }
+  // const PrevPage = () => {
+  //   console.log('Anterior pag.')
+  // }
 
   return (
     <div className='profile-container'>
@@ -103,7 +157,11 @@ const Recruiters = () => {
           />
           </div>          
           <div className="tableContainer">
-            <TableRecluter />
+            <TableRecluter postulants = {postulants} />
+            <div className="pagination">
+              <button className='btn-page' onClick={PrevPage}>Página anterior</button>
+              <button className='btn-page' onClick={NextPage}>Siguiente página</button>
+            </div>
           </div>
         </div>
        <SmallFooter />   
